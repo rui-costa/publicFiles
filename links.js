ec67319f9f9e6b74f6be1369f7fc65f8846a9a33
Rui Costa <44004884+rui-costa@users.noreply.github.com>
2021-04-12 17:03:05 +0200

    Update links.js

--- links.js
+++ links.js
@@ -2,7 +2,7 @@ function getRandomText(){
     return btoa(Math.floor(Math.random()*100000000000)+ 10000000000000000000000).replaceAll('=','').substring(1,20)
 }
 
-var TEAM_UNIQUE_LINK = prompt("Insert your Room Code\nCannot be longer than 20 chars\nYou can also use the auto-generated one", (getRandomText())); 
+var ROOM_CODE = prompt("Insert your Room Code\nCannot be longer than 20 chars\nYou can also use the auto-generated one", (getRandomText())); 
 const PASSWORD = prompt("Insert your Password\nYou can also use the auto-generated one", (getRandomText()));
 
 
@@ -27,7 +27,7 @@ function obsUrl(readWrite,object,iterator = 1){
         data_object = `_D${iterator}`
     }
 
-    return `https://obs.ninja/?${type}=${TEAM_UNIQUE_LINK}${data_object}&${pw}=${PASSWORD}`
+    return `https://obs.ninja/?${type}=${ROOM_CODE}${data_object}&${pw}=${PASSWORD}`
 
 }
 
