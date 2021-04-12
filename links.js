994f34b334b851e3502d26513a4d8884bbc8ad26
Rui Costa <44004884+rui-costa@users.noreply.github.com>
2021-04-12 15:22:36 +0200

    Create links.js

---
+++ links.js
@@ -0,0 +1,38 @@
+var TEAM_UNIQUE_LINK = prompt("Please add your unique team link\nCannot be longer than 20 chars", "MYTEAMDEMOLINK"); //cannot be 20 chars
+const PASSWORD = prompt("Add a password for added protection", "P@55W0rD_123!");
+
+
+function obsUrl(readWrite,object,iterator = 1){
+    let type = 'error_in_parameter'
+    let data_object = 'error_in_object'
+    let pw = 'error_in_parameter'
+    if (readWrite === 'r'){
+        type = 'view'
+        pw = 'pw'
+    }
+
+    if (readWrite === 'w'){
+        type = 'push'
+        pw = 'password'
+    }
+
+    if(object === 'camera'){
+        data_object = `_C${iterator}`
+    }
+    if(object === 'desktop'){
+        data_object = `_D${iterator}`
+    }
+
+    return `https://obs.ninja/?${type}=${TEAM_UNIQUE_LINK}${data_object}&${pw}=${PASSWORD}`
+
+}
+
+function mainShare(){
+    document.getElementById('camera').src = obsUrl('r','camera')
+    document.getElementById('desktop').src = obsUrl('r','desktop')
+}
+
+function mainInvite(){
+    document.getElementById('camera').href = obsUrl('w','camera')
+    document.getElementById('desktop').src = obsUrl('w','desktop')
+}
