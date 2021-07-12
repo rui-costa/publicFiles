cb58eed9373908bf8d6acc5abbd086e471f2993a
Rui Costa <44004884+rui-costa@users.noreply.github.com>
2021-07-12 16:19:28 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,13 @@
+{
+  "settingsReloadIntervalMinutes": 1,
+  "fullscreen": false,
+  "autoStart": false,
+  "lazyLoadTabs": false,
+  "websites": [
+    {
+      "url": "https://ingka.signalfx.com/#/dashboard/EtY4_NiAIAA?groupId=EtY5MkjAAAA&configId=EtY4_7hAEAA&startTime=-1d&endTime=Now",
+      "duration": 60,
+      "tabReloadIntervalSeconds": 15
+    }
+  ]
+}
